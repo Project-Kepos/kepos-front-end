ad03b9becdd160ee929a860668c9e2b9bee40238
d4vwdi <d4vwdi@gmail.com>
2024-04-04 11:04:00 -0300

    Alterando algumas propriedades do Title e do Button

--- src/pages/my-account/index.jsx
+++ src/pages/my-account/index.jsx
@@ -1,11 +1,22 @@
 import { Header } from '@components/header'
 import { Footer } from '@components/footer'
+import { PiArrowLeft } from 'react-icons/pi'
+import styles from './styles.module.css'
+import { Link } from 'react-router-dom'
+
 export function MyAccount (){
     
 return (
 
 
-        <title>Document</title>
+        <div>
+            <p className={styles.title}>
+            Adicionar estufa
+            </p>
+            <Link to='/dashboard' className={styles.btnVoltar}>
+               <PiArrowLeft size={48}/> Voltar               
+            </Link>
+        </div>
 
 )
 }
