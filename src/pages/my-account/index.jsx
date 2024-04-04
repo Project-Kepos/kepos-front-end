641cd7aca61d5369222d50a3b1eed8db1fa862e4
d4vwdi <d4vwdi@gmail.com>
2024-04-04 11:11:16 -0300

    Adicionando botão buscar

--- src/pages/my-account/index.jsx
+++ src/pages/my-account/index.jsx
@@ -1,6 +1,7 @@
 import { Header } from '@components/header'
 import { Footer } from '@components/footer'
 import { PiArrowLeft } from 'react-icons/pi'
+import { MdPodcasts } from 'react-icons/md'
 import styles from './styles.module.css'
 import { Link } from 'react-router-dom'
 
@@ -16,6 +17,11 @@ return (
             <Link to='/dashboard' className={styles.btnVoltar}>
                <PiArrowLeft size={48}/> Voltar               
             </Link>
+
+            <button className={styles.btnBuscar}>
+               <MdPodcasts size={48}/> Buscar               
+            </button>
+
         </div>
 
 )
