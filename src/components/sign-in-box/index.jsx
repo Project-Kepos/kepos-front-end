53a908769a69c5c170b2e203b0c663d2d481e97a
FATEC ZONA LESTE <alberto.abrao@fatec.sp.gov.br>
2024-04-04 10:44:48 -0300

    create login

---
+++ src/components/sign-in-box/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styles from './styles.module.css'
+
+const SignInbox = () => {
+  return (
+    <div className={styles.box}>
+      <div className={styles.textLogin}>Fazer Login</div>
+      <div className={styles.greenBox}>
+      <form onSubmit={(e) => handleSignIn(e)}>
+        <input type="email" id="email" />
+        <input type="senha" id="senha" />
+        <button>Logar</button>
+      </form>
+      </div>
+    </div>
+  )
+}
+
+export default SignInbox
