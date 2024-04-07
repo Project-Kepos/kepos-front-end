cf910e6d12a9776e0ed18731c18fcb80be005889
albmarques <alberto.abrao@fatec.sp.gov.br>
2024-04-07 20:24:30 -0300

    compare password function

--- src/components/sign-up-box/index.jsx
+++ src/components/sign-up-box/index.jsx
@@ -1,33 +1,55 @@
 import Button from '../button'
+import { useState } from "react";
 import CustomLink from '../custom-link'
 import InputText from '../inputText'
 import styles from './styles.module.css'
 
+
+
 const SignUpbox = () => {
+  const [password, setPassword] = useState("");
+  const [confPassword, setConfPassword] = useState("");
+  const [warning, setWarnig] = useState(false)
+ 
+  const handleComparassion = () =>{
+    if(password!=confPassword){
+    setPassword("")
+    setConfPassword("")
+    setWarnig(true)
+    }
+    else{
+      setWarnig(false)
+    }
+  }
   return (
+
     <div className={styles.box}>
       <div>
-        {' '}
         <div className={styles.textLogin}>Criar uma conta</div>
         <div className={styles.greenBox}>
-          <form onSubmit={() => {}}>
+          <form onSubmit={() => { }}>
             <div className={styles.intoBox}>
               <div className={styles.boxItem}>
-                <InputText type="text" placeholder="Usuário" id="email" />
+                <InputText type="text" placeholder="Usuário" id="user" />
               </div>
               <div className={styles.boxItem}>
-                <InputText type="email" placeholder="E-mail" id="senha" />
+                <InputText type="email" placeholder="E-mail" id="email" />
               </div>
               <div className={styles.boxItem}>
-                <InputText type="password" placeholder="Senha" id="senha" />
+                <InputText type="password" placeholder="Senha" id="senha" value={password} onChange={e => {setPassword(e.target.value);setWarnig(false)}} />
               </div>
               <div className={styles.boxItem}>
                 <InputText
                   type="password"
                   placeholder="Confirmar senha"
                   id="Confsenha"
+                  value={confPassword}
+                  onChange={e => {setConfPassword(e.target.value)}}
+                  onBlur={e =>{handleComparassion()}}
+                 
                 />
               </div>
+              {warning ? 'senhas não coicidem' : ''}
               <div className={styles.boxItem}>
                 <Button>Criar Conta</Button>
               </div>
