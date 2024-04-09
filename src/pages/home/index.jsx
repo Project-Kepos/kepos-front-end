2fc67e0f34f558de7f0b60ac774f23ae731b13c2
albmarques <alberto.abrao@fatec.sp.gov.br>
2024-04-09 16:59:17 -0300

    started menu and created dendro box component

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,14 @@
 import { Link } from 'react-router-dom'
-
+import DendroBox from '../../components/dendro-box'
+import styles from './styles.module.css'
 export function Home() {
   return (
     <main>
-      <h1>Menu com as estufas</h1>
-      <Link to="/dashboard/greenhouse/1">Dendro 1</Link>
+      <div className={styles.textMenu}>Menu com as estufas</div>
+      <div className={styles.menuContainer}>
+        <Link to="/dashboard/greenhouse/1"><DendroBox dendroName="Déndro 01"/></Link>
+        <DendroBox dendroName="Déndro 02"/>
+      </div>
     </main>
   )
 }
