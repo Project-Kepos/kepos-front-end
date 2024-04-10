93bbbaa436c5c41583048ed3d658794da6655998
albmarques <alberto.abrao@fatec.sp.gov.br>
2024-04-09 23:15:25 -0300

    improvements in menu page

--- src/components/dendro-box/index.jsx
+++ src/components/dendro-box/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import styles from './styles.module.css'
 import dendro from '@assets/dendro.svg'
+import { HiOutlineHome } from 'react-icons/hi';
 const DendroBox = ({dendroName}) => {
   return (
     <div className={styles.dendroBox}>
-        <div className={styles.dendroIcon}><img src={dendro}/></div>
+        <div className={styles.dendroIcon}><HiOutlineHome size={40} /></div>
         <div className={styles.dendroText}>{dendroName}</div>
     </div>
   )
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,15 +1,32 @@
 import { Link } from 'react-router-dom'
 import DendroBox from '../../components/dendro-box'
 import AddDendro from '../../components/add-dendro-box'
+import { MdAutorenew, MdClose } from 'react-icons/md'
 import styles from './styles.module.css'
 export function Home() {
   return (
     <main>
       <h2 className={styles.textMenu}>Menu com as estufas</h2>
       <div className={styles.menuContainer}>
-        <Link to="/dashboard/greenhouse/1"><DendroBox dendroName="Déndro 01"/></Link>
-        <DendroBox dendroName="Déndro 02"/>
+        <div className={styles.col}><Link to="/dashboard/greenhouse/1"><DendroBox dendroName="Déndro 01" /></Link></div>
+        <div className={styles.col}><DendroBox dendroName="Déndro 02" /></div>
+        <div className={styles.col}><DendroBox dendroName="Déndro 03" /></div>
+        <div className={styles.col}><DendroBox dendroName="Déndro 03" /></div>
+        <div className={styles.col}><DendroBox dendroName="Déndro 04" /></div>
+        <div className={styles.col}><DendroBox dendroName="Déndro 05" /></div>
         <AddDendro/>
+      </div>
+      <div className={styles.operations}>
+        <Link>
+          <div className={styles.oneOperation}>
+            <MdAutorenew size={46} />
+            Atualizar
+          </div>
+        </Link>
+        <div className={styles.oneOperation}>
+          <MdClose size={46} />
+          Desconectar
+        </div>
       </div>
     </main>
   )
