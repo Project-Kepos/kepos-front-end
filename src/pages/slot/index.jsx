8a01fa894d5784df383e5ed02e32bf55060ae8f5
albmarques <alberto.abrao@fatec.sp.gov.br>
2024-04-12 19:18:19 -0300

    Completed slot page

--- src/pages/slot/index.jsx
+++ src/pages/slot/index.jsx
@@ -2,17 +2,30 @@ import React from 'react'
 import styles from './styles.module.css'
 import { Link } from 'react-router-dom'
 import { MdArrowBack, MdAutorenew, MdClose } from 'react-icons/md'
-import { FaLeaf } from 'react-icons/fa';
+import { PiPottedPlant } from 'react-icons/pi'
+import { TbDroplets } from 'react-icons/tb'
 
 const Slot = () => {
   return (
+    <>
     <div className={styles.container}>
-        <Link to="/dashboard" className={styles.back}>
-          <MdArrowBack size={48} />
-          <div className={styles.backText}>Voltar</div>
-        </Link>
-        <div><FaLeaf size={75}/></div>
+      <Link to="/dashboard" className={styles.back}>
+        <MdArrowBack size={48} />
+        <div className={styles.backText}>Voltar</div>
+      </Link>
+      <div className={styles.plant}><PiPottedPlant size={250} /></div>
+      <div className={styles.firstInfos}>
+        <div className={styles.plantName}>Salsa</div>
+        <div className={styles.plantHumidityIcon}><TbDroplets /></div>
+        <div className={styles.plantHumidity}>89g/m³</div>
+      </div>      
     </div>
+    <div className={styles.description}>
+      <div className={styles.title}>Descrição</div>
+        <div>Lorem ipsum dolor sit amet consectetur. Auctor ultrices molestie diam pellentesque ipsum pellentesque viverra arcu duis. Duis quam in gravida morbi adipiscing scelerisque cursus bibendum.</div>
+    </div>
+    </>
+    
   )
 }
 
