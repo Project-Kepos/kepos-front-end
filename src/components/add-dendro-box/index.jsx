2b037b6156e98d6d09085428ddd678553071ed5c
d4vwdi <d4vwdi@gmail.com>
2024-04-14 12:44:59 -0300

    Adicionando Nova Estufa e estilizando

--- src/components/add-dendro-box/index.jsx
+++ src/components/add-dendro-box/index.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import styles from './styles.module.css'
 import plus from '@assets/plus.svg'
 import  {RiAddLine}  from 'react-icons/ri';
+import { Link } from 'react-router-dom';
 
 const AddDendro = () => {
   return (
+    <Link to='./add-greenhouse' className={styles.profile}>
     <div className={styles.dendroBox}>
         <div className={styles.dendroIcon}><RiAddLine size={40}/></div>
         <div className={styles.dendroText}><p>Adicionar </p>Estufa</div>
     </div>
+    </Link>
   )
 }
 
--- src/pages/add-greenhouse/index.jsx
+++ src/pages/add-greenhouse/index.jsx
@@ -8,20 +8,25 @@ import { Link } from 'react-router-dom'
 export function AddGreenhouse() {
 
    return (
-
-
       <div>
-         <p className={styles.title}>
-            Adicionar estufa
-         </p>
-         <Link to='/dashboard' className={styles.btnVoltar}>
-            <PiArrowLeft size={48} /> Voltar
-         </Link>
-
-         <button className={styles.btnBuscar}>
-            <MdPodcasts size={48} /> Buscar
-         </button>
-
+         <div className={styles.card}>
+            <p className={styles.title}>
+               Adicionar Estufa
+            </p>
+            <Link to='/dashboard' className={styles.btnVoltar}>
+               <PiArrowLeft size={48} /> Voltar
+            </Link>
+         </div>
+         <div className={styles.codigo}>
+            <p></p>
+            
+            <div>         
+               <form>
+               <label for="fname">Insira o código de <br></br>12 dígitos da sua estufa:</label><br></br>
+               <input type="text" id="form" name="form" placeholder='ABCD-EFGH-IJKL'  className={styles.form}/><br></br>
+            </form>
+            </div>   
+         </div>
       </div>
 
    )
