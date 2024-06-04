bde1573e6e4e8e1d9fe094c84e3c88049d33d995
albmarques <alberto.abrao@fatec.sp.gov.br>
2024-06-04 00:33:10 -0300

    Creted modal and patchs to greenhouse

--- src/pages/greenhouse/index.jsx
+++ src/pages/greenhouse/index.jsx
@@ -6,16 +6,31 @@ import { PiPottedPlant } from 'react-icons/pi'
 import { TbDroplets } from 'react-icons/tb'
 import { WiDaySunnyOvercast } from 'react-icons/wi'
 import { Link, useParams } from 'react-router-dom'
-
+import 'react-responsive-modal/styles.css';
+import { Modal } from 'react-responsive-modal';
 import styles from './styles.module.css'
+import { toast, ToastContainer, useToast } from 'react-toastify'
+import Cookies from 'js-cookie'
+import { useNavigate } from 'react-router-dom'
+import { AppError } from '@utils/AppError'
 
 const weatherMap = {
   sunny: 'Ensolarado',
 }
 
 const Greenhouse = () => {
+  const navigate = useNavigate()
   const [greenHouseData, setGreenHouseData] = useState({})
   const params = useParams()
+  const [campoNome, setCampoNome] = useState('')
+  const [openUpdate, setOpenUpdate] = useState(false);
+  const [openDisconnect, setOpenDisconnect] = useState(false);
+
+  const onOpenUpdateNameModal = () => setOpenUpdate(true);
+  const onCloseUpdateNameModal = () => setOpenUpdate(false);
+
+  const onOpenDisconnectModal = () => setOpenDisconnect(true);
+  const onCloseDisconnectModal = () => setOpenDisconnect(false);
 
   useEffect(() => {
     async function fetchGreenHouseData() {
@@ -26,12 +41,62 @@ const Greenhouse = () => {
           ...greenhouse.data,
           modules: [...modules.data],
         })
+        setCampoNome(greenhouse.data.name)
       } catch (error) {
         console.log(error)
       }
     }
     fetchGreenHouseData()
   }, [params.id])
+
+  async function handleChangeData(e) {
+    e.preventDefault()
+      try {
+        
+        await api.patch("/dendro/" + params.id, {
+          name:campoNome
+        })
+        setGreenHouseData({
+          ...greenHouseData,
+          name: campoNome
+        });
+        toast.success('nome atualizado')
+      } catch (error) {
+        console.log(error)
+        const isAppError = error instanceof AppError
+        const title = isAppError ? error.message : 'Erro no servidor.'
+        const description = isAppError
+          ? 'Verifique os dados e tente novamente.'
+          : 'Tente novamente mais tarde.'
+  
+        toast.error(title)
+      }
+
+    }
+
+    async function handleDisconnect(e) {
+      e.preventDefault()
+        try {
+          
+          await api.patch("/dendro/usuario/desassociar", {
+            id:params.id
+          })
+          console.error(e)
+          navigate('/dashboard', { replace: true, state: { useToast: true, toastMessage: "Estufa desconectada com sucesso" } });
+        } catch (error) {
+          console.log(error)
+          const isAppError = error instanceof AppError
+          const title = isAppError ? error.message : 'Erro no servidor.'
+          const description = isAppError
+            ? 'Verifique os dados e tente novamente.'
+            : 'Tente novamente mais tarde.'
+    
+          toast.error(title)
+        }
+  
+      }
+
+  
 
   return (
     <main className={styles.container}>
@@ -117,20 +182,60 @@ const Greenhouse = () => {
         <h2>Configurações da Estufa</h2>
 
         <div>
-          <button>
+          <button onClick={onOpenUpdateNameModal}>
             <FaRegEdit size={48} />
             Alterar nome da estufa
+            
           </button>
           <button>
             <MdAutorenew size={48} />
             Atualizar
           </button>
           <button>
-            <MdClose size={48} />
+            <MdClose size={48} onClick={onOpenDisconnectModal}/>
             Desconectar Estufa
           </button>
         </div>
+        <Modal open={openUpdate} onClose={onCloseUpdateNameModal} center 
+          classNames={{
+          modal: styles.modalContainer
+        }}> 
+        <h2>Digite um novo nome ao dendro</h2>
+        <form autoComplete={false} onSubmit={(e) => handleChangeData(e)}>
+        <p>
+        <input
+              type="text"
+              id="username"
+              value={campoNome}
+              onChange={(e) => {
+                setCampoNome(e.target.value);
+              }}
+            />
+        </p>
+        <button>Atualizar</button>
+        </form>
+      </Modal>
+      
+      <Modal open={openDisconnect} onClose={onCloseDisconnectModal}  center classNames={{
+          modal: styles.modalContainer
+        }}>
+        <h2 >Deseja realmente desconectar a estufa?</h2>
+        <p className={styles.margin}>Caso haja arrependimento, será possível reconectar!</p>
+        <button className={styles.cancelButton} onClick={handleDisconnect}>Confirmar</button><button onClick={onCloseDisconnectModal} >Cancelar</button>
+        </Modal>
       </section>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme={Cookies.get('toggle')==='true'? "dark":"light"}
+      />
     </main>
   )
 }
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,17 +1,21 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState} from 'react'
 import { MdAutorenew, MdClose } from 'react-icons/md'
-import { Link, useNavigate } from 'react-router-dom'
-
+import { Link, useNavigate,useLocation } from 'react-router-dom'
 import AddDendro from '../../components/add-dendro-box'
 import DendroBox from '../../components/dendro-box'
 import { api } from '../../libs/axios'
 import styles from './styles.module.css'
+import { toast, ToastContainer } from 'react-toastify'
+import Cookies from 'js-cookie'
 
 const Home = () => {
   const [userDendros, setUserDendros] = useState([])
   const navigate = useNavigate()
-
+  const location = useLocation();
   useEffect(() => {
+    if (location.state?.useToast) {
+      toast.success(location.state.toastMessage);
+    }
     async function fetchDendros() {
       try {
         const { data } = await api.get('/dendro/usuario')
@@ -21,7 +25,7 @@ const Home = () => {
       }
     }
     fetchDendros()
-  }, [])
+  }, [location])
 
   function handleLogout() {
     localStorage.clear('kepos-login')
@@ -49,6 +53,19 @@ const Home = () => {
           Desconectar
         </button>
       </div>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme={Cookies.get('toggle')==='true'? "dark":"light"}
+      />
+      
     </main>
   )
 }
