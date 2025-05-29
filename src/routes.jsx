f807861d7ad4f90f1046e86db5294122c22723a8
yRaposo <ynyght@gmail.com>
2025-05-28 21:21:46 -0300

    Revert ""port alter 8081""
    This reverts commit 74426273fb00e0377ffe235dc0c7775382747201.

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,25 +1,88 @@
-g>
-    <string name="notification_channel_wfc" msgid="9048240466765169038">"Wi-Fi 通話"</string>
-    <string name="notification_channel_sim" msgid="5098802350325677490">"SIM 卡狀態"</string>
-    <string name="notification_channel_sim_high_prio" msgid="642361929452850928">"重要 SIM 卡狀態"</string>
-    <string name="peerTtyModeFull" msgid="337553730440832160">"對方曾要求 TTY 完整模式"</string>
-    <string name="peerTtyModeHco" msgid="5626377160840915617">"對方曾要求 TTY 模式 (HCO)"</string>
-    <string name="peerTtyModeVco" msgid="572208600818270944">"對方曾要求 TTY 模式 (VCO)"</string>
-    <string name="peerTtyModeOff" msgid="2420380956369226583">"對方曾要求 TTY 模式 (OFF)"</string>
-    <string name="serviceClassVoice" msgid="2065556932043454987">"語音服務"</string>
-    <string name="serviceClassData" msgid="4148080018967300248">"數據"</string>
-    <string name="serviceClassFAX" msgid="2561653371698904118">"傳真"</string>
-    <string name="serviceClassSMS" msgid="1547664561704509004">"SMS"</string>
-    <string name="serviceClassDataAsync" msgid="2029856900898545984">"非同步"</string>
-    <string name="serviceClassDataSync" msgid="7895071363569133704">"Google Sync"</string>
-    <string name="serviceClassPacket" msgid="1430642951399303804">"封包"</string>
-    <string name="serviceClassPAD" msgid="6850244583416306321">"PAD"</string>
-    <string name="roamingText0" msgid="7793257871609854208">"漫遊指示開啟"</string>
-    <string name="roamingText1" msgid="5073028598334616445">"漫遊指示關閉"</string>
-    <string name="roamingText2" msgid="2834048284153110598">"漫遊指示閃爍"</string>
-    <string name="roamingText3" msgid="831690234035748988">"超出鄰近範圍"</string>
-    <string name="roamingText4" msgid="2171252529065590728">"超出建築物範圍"</string>
-    <string name="roamingText5" msgid="4294671587635796641">"漫遊 - 首選系統"</string>
-    <string name="roamingText6" msgid="5536156746637992029">"漫遊 - 可用系統"</string>
-    <string name="roamingText7" msgid="1783303085512907706">"漫遊 - 聯盟合作夥伴"</string>
-    <string name="roamingText8" msgid="77748007043
+import { createBrowserRouter } from 'react-router-dom'
+
+import AuthLayout from './layouts/auth-layout'
+import DefaultLayout from './layouts/default-layout'
+import Acessibilidade from './pages/acessibilidade'
+import AddGreenhouse from './pages/add-greenhouse'
+import Greenhouse from './pages/greenhouse'
+import Home from './pages/home'
+import LandingPage from './pages/landing-page'
+import MyAccount from './pages/my-account'
+import SignIn from './pages/sign-in'
+import SignUp from './pages/sign-up'
+import Slot from './pages/slot'
+
+export const router = createBrowserRouter([
+  // Rotas comuns (header e footer)
+  {
+    path: '/dashboard',
+    element: <DefaultLayout />,
+    children: [
+      {
+        // Menu principal com a listagem de estufa
+        path: '',
+        element: <Home />,
+      },
+      {
+        // Menu da estufa (Minha tela)
+        path: 'greenhouse/:id',
+        element: <Greenhouse />,
+      },
+      {
+        // Menu busca da estufa
+        path: 'greenhouse/:id',
+        element: <Greenhouse />,
+      },
+      {
+        // Dashboard Nova Estufa
+        path: 'add-greenhouse',
+        element: <AddGreenhouse />,
+      },
+      {
+        // Dashboard Minha Conta
+        path: 'my-account',
+        element: <MyAccount />,
+      },
+      {
+        // Dashboard do slot
+        path: 'slot/:id',
+        element: <Slot />,
+      },
+      {
+        // Acessibilidade
+        path: 'acessibilidade',
+        element: <Acessibilidade />,
+      },
+    ],
+  },
+
+  // Rotas login
+  // A header vai precisar colocar manualmente nas telas de login e cadastro <Header />
+  // O footer já tem no layout
+  {
+    path: '/',
+    element: <AuthLayout />,
+    children: [
+      {
+        // Primeira página de todas
+        path: '/',
+        element: <LandingPage />,
+      },
+      {
+        // Login
+        path: '/sign-in',
+        element: <SignIn />,
+      },
+      {
+        // Cadastro
+        path: '/sign-up',
+        element: <SignUp />,
+      },
+      {
+        // Acessibilidade
+        path: 'acessibilidade',
+        element: <Acessibilidade />,
+      },
+    ],
+  },
+])
